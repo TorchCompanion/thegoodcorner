0ae79b52f4ce89cbebf9531d868bb4a67ce04792
TorchCompanion <picaut.alan@gmail.com>
2023-01-02 09:08:37 +0100

    Added Login form, polished annonce form, added register form

---
+++ src/Type/UserType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'First names',
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Last name',
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+            ])
+            ->add('phone_number', TelType::class, [
+                'label' => 'Phone number',
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Password',
+                'attr' => ['class' => 'form-control'],
+                'label_attr' => ['class' => 'form-label'],
+            ])
+            ->add('newsletter', CheckboxType::class, [
+                'label' => 'Newsletter',
+                'attr' => ['class' => 'form-check-input'],
+                'label_attr' => ['class' => 'form-check-label'],
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Post annonce',
+                'attr' => ['class' => 'btn btn-primary'],
+            ]);
+    }
+}
